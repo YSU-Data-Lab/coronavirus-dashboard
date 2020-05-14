ceea8304990fc4bb6da60fcb8ee5b9de02f8cdf8
Feng Yu <fyu@ysu.edu>
2020-05-14 14:54:44 -0400

    [add] 7 day average new cases

--- visualization/python/visualize_daily.py
+++ visualization/python/visualize_daily.py
@@ -13,6 +13,7 @@ rotation_degree=90
 data_file_name = '../../data/ohio.json'
 num_cases_file_name='../../figure/num_cases.svg'
 num_new_cases_file_name='../../figure/num_new_cases.svg'
+num_new_avg_7d_cases_file_name='../../figure/num_new_avg_7d_cases.svg' # number of average new cases in previous 7 days
 # num_counties_file_name='../../figure/num_counties.svg'
 num_icu_file_name='../../figure/num_icu.svg'
 num_hospitalizations_file_name='../../figure/num_hospitalizations.svg'
@@ -27,8 +28,9 @@ else:
     daily_entries=[]
     
 df = pd.DataFrame(daily_entries)
-df['num_new_cases']=df['num_cases'].diff() # add newly confirmed cases
 
+
+## num of total cases
 x=df['date']
 y=df['num_cases']
 # plt.figure(figsize=(fig_width, fig_height))
@@ -50,7 +52,8 @@ plt.clf()
 plt.cla()
 plt.close()
 
-
+## num of new cases
+df['num_new_cases']=df['num_cases'].diff() # add newly confirmed cases
 x=df['date']
 z=df['num_new_cases']
 plt.title('Newly Confirmed Cases in Ohio')
@@ -68,7 +71,26 @@ plt.clf()
 plt.cla()
 plt.close()
 
+# number of average new cases in previous 7 days
+df['num_new_avg_7d_cases']=df['num_cases'].diff(7)/7 # add newly confirmed cases
+x=df['date']
+z=df['num_new_avg_7d_cases']
+plt.title('7-Day Average Newly Confirmed Cases in Ohio')
+plt.plot(x, z, marker='.', markersize=12, color='yellow', linewidth=2, label='Avg Daily New in Last 7 Days')
+# plt.legend()
+bottom, top = plt.ylim()
+plt.ylim(0, top*1.1)
+plt.xticks(rotation=rotation_degree)
+plt.gca().yaxis.set_major_locator(ticker.MaxNLocator(integer=True))
+for i,j in zip(x[8:],z[8:]):
+    plt.text(i, j, str(int(j)), ha='center', va='bottom')
+plt.tight_layout()
+plt.savefig(num_new_avg_7d_cases_file_name)
+plt.clf()
+plt.cla()
+plt.close()
 
+## num of total icu counts
 y=df['num_icu'].dropna() # delete NaN entries
 x=df['date'][len(df['date'])-len(y):] # subarray of dates with num_icu available
 # plt.figure(figsize=(fig_width, fig_height))
